a878c36272bc511eceeb0e771494e50aedcfad99
Nayab Kara <nayab.kara@gmail.com>
2022-05-27 15:41:27 -0700

    fixing key

--- wackywidgets/settings.py
+++ wackywidgets/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*6xx*sf4)otr1*wx!si_z6b(domad^5zifj5@kl+!lz@-8@pvn'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
